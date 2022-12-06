2d398daae8b8f75118d2d3fa8a3859157bf0c483
Johnnyboy-24 <nummer24.jb@gmail.com>
2022-12-06 09:50:39 +0100

    Initial commit

---
+++ StegProg/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StegProg
+{
+    public class Program
+    {
+        Bitmap bitmap;
+        Image image;
+
+        static void Main(string[] args)
+        {
+            
+            
+        }
+        
+
+        
+    }
+}
